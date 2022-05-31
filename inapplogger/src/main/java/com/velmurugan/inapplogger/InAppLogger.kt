be55b6dbb8946af344531d22045df86317714617
Norbel Ambanumben <aanorbel@gmail.com>
2022-05-31 17:09:41 +0100

    Modified log display and reduced minimum sdk.

--- inapplogger/src/main/java/com/velmurugan/inapplogger/InAppLogger.kt
+++ inapplogger/src/main/java/com/velmurugan/inapplogger/InAppLogger.kt
@@ -1,6 +1,8 @@
 package com.velmurugan.inapplogger
 
 import android.content.Context
+import android.text.format.DateFormat
+import java.util.*
 
 class InAppLogger(context: Context) : LoggerManager(context) {
 
@@ -24,19 +26,19 @@ class InAppLogger(context: Context) : LoggerManager(context) {
 
 
     override fun e(msg: String) {
-        saveLog("<font color=#cc0029>${ LogType.ERROR.name} : $msg</font>",LogType.ERROR.name)
+        saveLog("<font color=#cc0029>${DateFormat.format("yyyy-MM-dd hh:mm:ss a", Date())} : ${ LogType.ERROR.name} : $msg</font>",LogType.ERROR.name)
     }
 
     override fun w(msg: String) {
-        saveLog("<font color=#FFC107>${LogType.WARNING.name} : $msg</font>",LogType.WARNING.name)
+        saveLog("<font color=#FFC107>${DateFormat.format("yyyy-MM-dd hh:mm:ss a", Date())} : ${LogType.WARNING.name} : $msg</font>",LogType.WARNING.name)
     }
 
     override fun i(msg: String) {
-        saveLog("<font color=#2196F3>${LogType.INFO.name} : $msg</font>", LogType.INFO.name)
+        saveLog("<font color=#2196F3>${DateFormat.format("yyyy-MM-dd hh:mm:ss a", Date())} : ${LogType.INFO.name} : $msg</font>", LogType.INFO.name)
     }
 
     override fun api(msg: String) {
-        saveLog("<font color=#C1C1C1>${LogType.API.name} : $msg</font>", LogType.API.name)
+        saveLog("<font color=#C1C1C1>${DateFormat.format("yyyy-MM-dd hh:mm:ss a", Date())} : ${LogType.API.name} : $msg</font>", LogType.API.name)
     }
 
 
